f61b84c8ce8a2954f9335e286a17f8aa4aea276c
Khariza <kharizzakaye@gmail.com>
2025-03-05 17:53:34 +0800

    Connect to supabase (postgresql)

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,12 +1,17 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
+import { connectToDB } from "@/app/lib/data";
 
-export default function Page () {
+export default async function Page () {
+
+    const client = await connectToDB();
+
     return(
         <>
+            { client && <p className="text-green-500">Connected to the database</p> }
+
             <h1 className="text-purple-800">Posts</h1>
 
-        
                 {posts.map((post) => (
                     <Post key={post.id} {...post} />
                 ))}
